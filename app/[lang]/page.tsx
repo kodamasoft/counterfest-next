b8fe14ae8be5f4a6698ad7ece4670b8b61c14b10
Viviane <dessaintviviane@gmail.com>
2024-11-13 17:18:07 +0100

    Refactor layout and localization: remove old files and implement new structure with language support

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import IndexMDX from '@/markdown/index.mdx'
+import logo from '@/public/img/Logo_Orange_Transparent.png'
+import logoDark from '@/public/img/Logo_DarkOrange_Transparent.png'
+
+export default function Home() {
+    return (
+        <div className="prose dark:prose-invert max-w-3xl mx-auto p-4">
+            <Image src={logo} alt="COUNTERFEST RECORDS" width={300} height={288} className="mx-auto animate-fade-in-down mt-4 mb-8 hidden dark:block" />
+            <Image src={logoDark} alt="COUNTERFEST RECORDS" width={300} height={288} className="mx-auto animate-fade-in-down mt-4 mb-8 dark:hidden" />
+
+            <IndexMDX />
+        </div>
+    );
+}

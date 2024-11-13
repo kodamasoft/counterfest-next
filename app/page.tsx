fd2d6694bcee0a992421821a99ba6b1e8e4c51ee
krz <maxlav78@gmail.com>
2024-11-13 15:04:59 +0100

    Simplify prose classes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import IndexMDX from '@/markdown/index.mdx'
 
 export default function Home() {
     return (
-        <div className="sm:prose-sm md:prose-lg dark:prose-dark max-w-3xl mx-auto p-4">
+        <div className="prose dark:prose-dark max-w-3xl mx-auto p-4">
             <Image src="/img/Logo_Orange_Transparent.png" alt="COUNTERFEST RECORDS" width={300} height={288} className="mx-auto animate-fade-in-down" />
             <IndexMDX />
         </div>

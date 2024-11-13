314d694a60ce68020addf4041dc236ffb230d2e7
krz <maxlav78@gmail.com>
2024-11-13 15:12:49 +0100

    Adjust margins and prose dark style

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import IndexMDX from '@/markdown/index.mdx'
 
 export default function Home() {
     return (
-        <div className="prose dark:prose-dark max-w-3xl mx-auto p-4">
-            <Image src="/img/Logo_Orange_Transparent.png" alt="COUNTERFEST RECORDS" width={300} height={288} className="mx-auto animate-fade-in-down" />
+        <div className="prose dark:prose-invert max-w-3xl mx-auto p-4">
+            <Image src="/img/Logo_Orange_Transparent.png" alt="COUNTERFEST RECORDS" width={300} height={288} className="mx-auto animate-fade-in-down mt-4 mb-8" />
             <IndexMDX />
         </div>
     );

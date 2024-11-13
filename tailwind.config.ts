4bd57762a80112cb5125df93285071335cb32f9f
krz <maxlav78@gmail.com>
2024-11-13 14:52:47 +0100

    Setup landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from '@tailwindcss/typography';
 
 export default {
     content: [
@@ -6,13 +7,26 @@ export default {
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
+    plugins: [
+        typography,
+    ],
     theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
+        extend: {        
+            animation: {
+                "fade-in-down": "fade-in-down 0.6s ease-in-out"
             },
+            keyframes: {
+                "fade-in-down": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "translateY(-20px)"
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "translateY(0)"
+                    }
+                }
+            }
         },
     },
-    plugins: [],
 } satisfies Config;

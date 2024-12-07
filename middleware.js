2db67e23f76e9140b63d6aa5710ebafcce8d2dbf
krz <maxlav78@gmail.com>
2024-12-07 21:48:21 +0100

    Update middleware config to exclude additional paths from matcher

--- middleware.js
+++ middleware.js
@@ -26,6 +26,7 @@ export function middleware(request) {
 
 export const config = {
   matcher: [
-    '/((?!_next|admin).*)',
+    // '/((?!_next|admin|_next/image|favicon.ico).*)',
+    "/((?!api|_next/static|_next/image|admin|assets|icons|favicon.ico).*)"
   ],
 };

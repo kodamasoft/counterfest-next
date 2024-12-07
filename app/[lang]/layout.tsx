f95001c3da2a4581e2dcebee6fe2d5c3f74fffa6
Viviane <dessaintviviane@gmail.com>
2024-12-07 05:42:29 +0100

    Update RootLayout to use a Promise for language parameters

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
     params
 }: Readonly<{
     children: React.ReactNode;
-    params: { lang: string };
+    params: Promise<{ lang: string }>
 }>) {
     const { lang } = await params;
     return (

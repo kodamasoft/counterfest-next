4243736b3d450ee4fb9fe6e3439cac18a37bc7d1
Viviane <dessaintviviane@gmail.com>
2024-12-21 02:33:50 +0100

    Refactor LocaleSwitcher component to accept language prop; update header to include LocaleSwitcher with new styles

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import { WEBSITE_NAME } from '../lib/constants'
-//import LocaleSwitcher from './locale-switcher'
+import LocaleSwitcher from './locale-switcher'
 import headerStyles from './header.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -21,24 +21,25 @@ export default async function Header({params}) {
 		// { href: '/blog', label: 'Blog' }
 	];
 
-	let canDisplayBanner = new Date().getMonth() <= 3 && new Date().getFullYear() === 2024;
+	//KodamaDirect 2024 banner
+	/*let canDisplayBanner = new Date().getMonth() <= 3 && new Date().getFullYear() === 2024;*/
 
 	return (
 		<header className="relative w-100 h-auto text-white text-center flex flex-col select-none bg-center bg-cover">
 
-			{
+			{/*
 				canDisplayBanner &&
 				<Link className="bg-purple-900 text-white p-2" href="/posts/kodamadirect-2024">
 					<p className="text-sm">
 						{/*t('common:kodamadirect2024_banner')*/}
-					</p>
+					{/*</p>
 					<button id="close" className="absolute top-0 right-0 p-2 text-white" onClick={() => document.getElementById('close').parentElement.style.display = 'none'} />
 				</Link>
-			}
+			*/}
 
 			<h1 className={headerStyles.header_title}>
 				<Link href="/" className="cursor-pointer">
-					<Image src={logo} alt={WEBSITE_NAME} height={150} className="cursor-pointer mx-auto max-w-full drop-shadow" />
+					<Image src={logo} alt={WEBSITE_NAME} height={250} className="cursor-pointer mx-auto max-w-full drop-shadow" />
 				</Link>
 			</h1>
 
@@ -85,16 +86,16 @@ export default async function Header({params}) {
 
 
 			{/* DESKTOP NAVBAR */}
-			<nav className="bg-purple-900/30 backdrop-blur text-white hidden sm:block">
+			<nav className="bg-orange-500/80 backdrop-blur hidden sm:block">
 				<div id="navbar" className="flex flex-col sm:flex-row justify-center p-4 gap-4 items-center">
 
 					{links.map((link, index) => (
-						<Link href={link.href} key={index} className="text-gray-300 hover:bg-purple-700/50 hover:text-white px-3 py-2 rounded-md text-sm font-medium border-2 border-transparent focus-visible:border-purple-600 focus-visible:outline-none transition">
+						<Link href={link.href} key={index} className="text-black hover:bg-black/80 hover:text-white px-3 py-2 rounded-md text-sm font-medium border-2 border-transparent focus-visible:border-purple-600 focus-visible:outline-none transition">
 							{link.label}
 						</Link>
 					))}
 
-					{/*<LocaleSwitcher className="w-fit m-0 text-gray-300 bg-gray-900/50 hover:bg-purple-700/50 hover:text-white px-3 py-2 rounded-md text-sm font-medium border-2 border-transparent focus-visible:border-purple-600 focus-visible:outline-none transition"></LocaleSwitcher>*/}
+					<LocaleSwitcher lang={lang}></LocaleSwitcher>
 				</div>
 			</nav>
 		</header>
--- components/locale-switcher.js
+++ components/locale-switcher.js
@@ -1,17 +1,17 @@
+"use client"
+
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
-export default function LocaleSwitcher({className}) {
-	const { locale, locales, asPath } = useRouter()
-
+export default function LocaleSwitcher({lang}) {
+    const locales = ['en', 'jp']
     return (
-        <div className={className}>
+        <div className="w-fit m-0 text-black bg-white/50 hover:bg-black/80 hover:text-white px-3 py-2 rounded-md text-sm font-medium border-2 border-transparent focus-visible:border-purple-600 focus-visible:outline-none transition">
             <div className="flex flex-row flex-wrap justify-center gap-4">
                 {locales.map((l, i) => {
                     return (
-                        <span key={i} className={l === locale ? 'font-bold' : ''}>
+                        <span key={i} className={l === lang ? 'font-bold' : ''}>
                             {/* to uppercase locale*/}
-                            <Link href={asPath} locale={l}>
+                            <Link href={`/${l}`} locale={l}>
                                 {l.toUpperCase()}
                             </Link>
                         </span>

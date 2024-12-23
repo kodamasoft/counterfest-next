ffa7d21f5a1f6e07111a017e6ac423b296a33c5e
Viviane <dessaintviviane@gmail.com>
2024-12-23 05:43:36 +0100

    Refactor FAQ page to support dynamic language loading and update Japanese content

--- app/[lang]/faq/page.tsx
+++ app/[lang]/faq/page.tsx
@@ -4,7 +4,8 @@ import dynamic from 'next/dynamic'
 //Waiting for framer motion update compatibility with NextJS 15
 //import WrappableParagraph from '@/components/project/title-with-content'
 
-export default async function page() {
+export default async function page({params}) {
+	const { lang } = await params;
 	const FaqMDX = dynamic(() => import(`@/markdown/faq.${lang}.mdx`));
 	return (
 		<div className="prose dark:prose-invert max-w-3xl mx-auto p-4">

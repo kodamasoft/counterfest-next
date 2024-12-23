b13d9b6d71fd420ba261d27e762ac3c1f369eb33
Viviane <dessaintviviane@gmail.com>
2024-12-23 05:39:44 +0100

    Refactor localization handling in FAQ page and add Japanese FAQ content

--- app/[lang]/faq/page.tsx
+++ app/[lang]/faq/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
 
-function page() {
+//Waiting for framer motion update compatibility with NextJS 15
+//import WrappableParagraph from '@/components/project/title-with-content'
+
+export default async function page() {
+	const FaqMDX = dynamic(() => import(`@/markdown/faq.${lang}.mdx`));
 	return (
-		<div>page</div>
+		<div className="prose dark:prose-invert max-w-3xl mx-auto p-4">
+			<FaqMDX />
+		</div>
 	)
 }
-
-export default page
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,7 @@
 import dynamic from 'next/dynamic';
-import { getDictionary } from './dictionaries'
 
 export default async function Home({params}) {
     const { lang } = await params;
-    const dict = await getDictionary(lang);
     const IndexMDX = dynamic(() => import(`@/markdown/index.${lang}.mdx`));
     // Use {dict.title} corresponding to the title in the dictionary
     

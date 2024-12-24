437683a6dbce14313a5eea3484fd1303c45ae7bc
Viviane <dessaintviviane@gmail.com>
2024-12-24 17:46:54 +0100

    Refactor FAQ page layout to enhance structure and readability

--- app/[lang]/faq/page.jsx
+++ app/[lang]/faq/page.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 
 import WrappableParagraph from '@/components/project/title-with-content'
+import Container from '@/components/container';
+import ProseContainer from '@/components/prose-container';
 
 export default async function Faq({params}) {
 	const { lang } = await params;
 	const FaqMDX = dynamic(() => import(`@/markdown/faq.${lang}.mdx`));
 	return (
-		<div className="prose dark:prose-invert max-w-3xl mx-auto p-4">
-			<FaqMDX />
-		</div>
+		<Container>
+			<ProseContainer>
+				<div className="max-w-3xl mx-auto p-4">
+					<FaqMDX components={{ WrappableParagraph }} />
+				</div>
+			</ProseContainer>
+		</Container>
 	)
 }
